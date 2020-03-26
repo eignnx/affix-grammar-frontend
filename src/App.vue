1f2af2cc3bf8edbbdbd2c4c27114f9c2bbb8a53a
eignnx <eignnx@gmail.com>
2020-03-26 16:09:41 -0400

    extract navigation drawer into own component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,6 @@
 <template>
   <v-app id="affix-grammar-app">
-    <v-navigation-drawer v-model="drawer" app clipped>
-      <v-list dense>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-settings</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app clipped-left>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Affix Grammar</v-toolbar-title>
-    </v-app-bar>
+    <NavigationDrawer />
 
     <v-content>
       <v-container fluid>
@@ -103,6 +79,7 @@
 
 <script>
 import Editor from "./components/Editor.vue";
+import NavigationDrawer from "./components/NavigationDrawer.vue";
 let ParserContext = null;
 import("affix-grammar-js").then(module => {
   ParserContext = module.ParserContext;
@@ -186,6 +163,6 @@ export default {
     }
   },
 
-  components: { Editor }
+  components: { Editor, NavigationDrawer }
 };
 </script>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-group v-model="savesOpen" prepend-icon="mdi-content-save" no-action>
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title v-text="'Saves'"></v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item v-for="save in saves" :key="save.key">
+            <v-list-item-content>
+              <v-list-item-title v-text="save.title"></v-list-item-title>
+              <v-row>
+                <v-col>
+                  <span class="caption">{{ save.modified }}</span>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                      <v-btn icon color="yellow" v-on="on">
+                        <v-icon>mdi-file-edit</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Edit</span>
+                  </v-tooltip>
+                </v-col>
+                <v-col>
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                      <v-btn icon color="info" v-on="on">
+                        <v-icon>mdi-file-download</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Download</span>
+                  </v-tooltip>
+                </v-col>
+                <v-col>
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                      <v-btn icon color="error" v-on="on">
+                        <v-icon>mdi-delete-forever</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Delete</span>
+                  </v-tooltip>
+                </v-col>
+              </v-row>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>Affix Grammar</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavigationDrawer",
+
+  data: () => ({
+    drawer: false,
+    savesOpen: false,
+    saves: [
+      { title: "Test Grammar 1", key: "grammar1", modified: "Three days ago" },
+      {
+        title: "English Sentence",
+        key: "english-sentence",
+        modified: "Feb 29, 2020"
+      }
+    ],
+    examplesOpen: false
+  })
+};
+</script>
+
+<style></style>
